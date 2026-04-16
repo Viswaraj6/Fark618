21586ac20e21f9d1453f7e69ce2b7e7fa4185d03
Viswaraj6 <75193269+Viswaraj6@users.noreply.github.com>
2026-04-17 02:19:41 +0530

    Update server.js

--- server.js
+++ server.js
@@ -2,13 +2,24 @@ const express = require("express");
 const cors = require("cors");
 
 const app = express();
-
 app.use(cors());
 
 app.get("/products", (req, res) => {
   res.json([
-    { id: 1, name: "Blue Shirt", price: 799 },
-    { id: 2, name: "Black T-Shirt", price: 499 }
+    {
+      id: 1,
+      name: "Blue Shirt",
+      price: 799,
+      stock: 10,
+      image: "https://via.placeholder.com/150"
+    },
+    {
+      id: 2,
+      name: "Black T-Shirt",
+      price: 499,
+      stock: 20,
+      image: "https://via.placeholder.com/150"
+    }
   ]);
 });
 
