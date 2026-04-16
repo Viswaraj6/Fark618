db80df6ebf51bee761d73fa28768a5d4a1564ced
Viswaraj6 <75193269+Viswaraj6@users.noreply.github.com>
2026-04-17 02:36:09 +0530

    Update server.js

--- server.js
+++ server.js
@@ -11,28 +11,28 @@ app.get("/products", (req, res) => {
       name: "Blue Shirt",
       price: 799,
       stock: 10,
-      image: "https://via.placeholder.com/300x200?text=Blue+Shirt"
+     image: "https://images.unsplash.com/photo-1602810318383-e386cc2a3ccf?w=500"
     },
     {
       id: 2,
       name: "Black T-Shirt",
       price: 499,
       stock: 20,
-image: "https://via.placeholder.com/300x200?text=Blue+Shirt"
+image: "https://images.unsplash.com/photo-1602810318383-e386cc2a3ccf?w=500"
     },
     {
       id: 3,
       name: "White Shirt",
       price: 899,
       stock: 5,
-      image: "https://via.placeholder.com/300x200?text=Blue+Shirt"
+      image: "https://images.unsplash.com/photo-1602810318383-e386cc2a3ccf?w=500"
     },
     {
       id: 4,
       name: "Grey Hoodie",
       price: 1299,
       stock: 8,
-      image: "https://via.placeholder.com/300x200?text=Grey+Hoodie"
+image: "https://images.unsplash.com/photo-1602810318383-e386cc2a3ccf?w=500"
     }
   ]);
 });
