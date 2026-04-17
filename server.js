2db72556ce6d71fe5a80364de79e45fec7f62103
Viswaraj6 <75193269+Viswaraj6@users.noreply.github.com>
2026-04-17 08:32:16 +0530

    Update server.js

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(express.json());
 app.use(cors());
 
 // 🔴 MongoDB Connection (IMPORTANT: replace password if needed)
-mongoose.connect("mongodb+srv://viswaraj6_db_user:IAkwXvTcUbIkrrEl@cluster0.n3jqyc9.mongodb.net/?appName=Cluster0")
+mongoose.connect("mongodb+srv://viswaraj6_db_user:IAkwXvTcUbIkrrEl@cluster0.n3jqyc9.mongodb.net/?retryWrites=true&w=majority")
 .then(() => console.log("MongoDB Connected ✅"))
 .catch(err => console.log(err));
 
