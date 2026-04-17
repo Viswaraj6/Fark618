3803c6143db55d4fa9138d30ed64abddf4b2944f
Viswaraj6 <75193269+Viswaraj6@users.noreply.github.com>
2026-04-17 09:54:27 +0530

    Update server.js

--- server.js
+++ server.js
@@ -102,11 +102,15 @@ app.post("/order", async (req, res) => {
 
 
 // 📦 GET ORDERS
-app.get("/orders", async (req, res) => {
+app.get("/products", async (req, res) => {
   try {
-    const data = await Order.find();
+    const data = await Product.find();
+
+    console.log("Products from DB:", data); // 🔥 add this
+
     res.json(data);
   } catch (err) {
+    console.log(err);
     res.status(500).send(err);
   }
 });
